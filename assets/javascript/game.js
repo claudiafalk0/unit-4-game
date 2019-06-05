5bbaf5a8299d351d270fab373b4f703e90bc70f6
Claudia Falk <claudiafalk0@gmail.com>
2019-06-04 20:24:07 -0600

    updated js code to properly display variables for individual images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,15 @@ var losses = 0;
 var Goal;
 var Score;
 var fruitValue;
+// var imagesArray = [];
+// imagesArray[0] = new Image();
+// imagesArray[0].src = "assets/images/apple.jpg"
+// imagesArray[1] = new Image();
+// imagesArray[1].src = "assets/images/banana.jpg"
+// imagesArray[2] = new Image();
+// imagesArray[2].src = "assets/images/lemon.jpg"
+// imagesArray[3] = new Image();
+// imagesArray[3].src = "assets/images/cherries.jpg"
 
 //wins
 //losses
@@ -12,45 +21,57 @@ var fruitValue;
 // Value of each crystal
 
 //create a function to start/reset game
+//Reset score to 0
+//Update score on the page
+
 function resetGame(){
     //create a new random number between 19 and 120
     //Update this on the page
-    Goal = Math.floor(Math.random()*101)+19;
+    Goal = [Math.floor(Math.random() * 101) + 19];
     $("#ComputerNumber").text(Goal);
     console.log(Goal);
     //create new values for each crystal between 1 and 12
-    fruitValue = Math.floor(Math.random()*12)+1;
+    // fruitValue = [Math.floor(Math.random()*12)+1];
+    
     //Assign each value to a crystal
-    for(var i = 1; i < 4; i++){
-        
-    }
-    Score = 0;
+    $(".fruit").each(function() {
+        fruitValue = Math.floor(Math.random() * 12) + 1;
+        $(this).attr("data-fruitvalue", fruitValue);
+    });
+
+    Score = 0
 }
 
-if(Score === Goal){
-    wins++;
-    resetGame();
-} else if (Score > Goal){
-    losses++;
-    resetGame();
-}
-    //Update losses on the page
-    //Update wins on the page
-    //Reset score to 0
-    //Update score on the page
-
-//Create a function to test win/lose
-    //If score === goal
-         //wins++
-         //call reset function
-    //If score > goal
-         //losses++
-         //call reset function
+resetGame();
 
 //create a click event 
+$(".fruit").on("click", function (){
     //Grab the value of the crystal clicked
     //Add that to the player's score
     //Test for win/loss
+    Score += parseInt($(this).attr("data-fruitvalue"));
+    console.log($(this).attr("data-fruitvalue"));
+    $("#userNumber").text(Score);
+    console.log($(this).attr("id"));
+    
+    //Update wins on the page
+    //Create a function to test win/lose
+    //If score === goal
+    //wins++
+    //call reset function
+    if(Score === Goal){
+        wins++;
+        resetGame();
+        //Update losses on the page
+        //If score > goal
+        //losses++
+        //call reset function
+    } else if (Score > Goal){
+        losses++;
+        resetGame();
+    }
+})
+
         //If score === goal
             //wins++
             //call reset function
